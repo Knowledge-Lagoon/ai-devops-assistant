94a710c8d7415315790742010baf78704266326f
Hussain Yandrapally <hussain.yandrapally@accenture.com>
2026-08-14 00:59:59 +1000

    s

--- app/runbook_service/confluence_search.py
+++ app/runbook_service/confluence_search.py
@@ -6,15 +6,50 @@ from app.runbook_service.confluence_client import (
     ATLASSIAN_API_TOKEN
 )
 
-url = f"{CONFLUENCE_URL}/api/v2/folders"
 
-response = requests.get(
-    url,
-    auth=(
-        ATLASSIAN_EMAIL,
-        ATLASSIAN_API_TOKEN
+def find_page_by_title(
+    title: str
+):
+
+    url = f"{CONFLUENCE_URL}/api/v2/pages"
+
+    response = requests.get(
+        url,
+        auth=(
+            ATLASSIAN_EMAIL,
+            ATLASSIAN_API_TOKEN
+        )
     )
-)
 
-print(response.status_code)
-print(response.text)
+    response.raise_for_status()
+
+    data = response.json()
+
+    for page in data.get(
+        "results",
+        []
+    ):
+
+        if (
+            page["title"].lower()
+            == title.lower()
+        ):
+
+            return {
+                "id": page["id"],
+                "title": page["title"],
+                "parent": page.get(
+                    "parentId"
+                )
+            }
+
+    return None
+
+
+if __name__ == "__main__":
+
+    result = find_page_by_title(
+        "CrashLoopBackOff"
+    )
+
+    print(result)
