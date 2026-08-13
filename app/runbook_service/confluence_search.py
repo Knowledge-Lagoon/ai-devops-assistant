bcaf9c22230c3a9d4f3d65285267a161fb34aaf4
Hussain Yandrapally <hussain.yandrapally@accenture.com>
2026-08-13 23:59:28 +1000

    d

--- app/runbook_service/confluence_search.py
+++ app/runbook_service/confluence_search.py
@@ -7,24 +7,64 @@ from app.runbook_service.confluence_client import (
 )
 
 
-url = f"{CONFLUENCE_URL}/api/v2/pages"
-
-response = requests.get(
-    url,
-    auth=(
-        ATLASSIAN_EMAIL,
-        ATLASSIAN_API_TOKEN
-    )
-)
-
-data = response.json()
-
-for page in data.get(
-    "results",
-    []
+def find_page_by_title(
+    title: str
 ):
 
-    print(
-        page["title"],
-        page["id"]
+    url = f"{CONFLUENCE_URL}/api/v2/pages"
+
+    response = requests.get(
+        url,
+        auth=(
+            ATLASSIAN_EMAIL,
+            ATLASSIAN_API_TOKEN
+        )
     )
+
+    response.raise_for_status()
+
+    data = response.json()
+
+    for page in data.get(
+        "results",
+        []
+    ):
+
+        if (
+            page["title"].lower()
+            == title.lower()
+        ):
+
+            return {
+                "id": page["id"],
+                "title": page["title"]
+            }
+
+    return None
+
+
+if __name__ == "__main__":
+
+    result = find_page_by_title(
+        "CrashLoopBackOff"
+    )
+
+    if result:
+
+        print(
+            "\nRunbook Found\n"
+        )
+
+        print(
+            f"Title: {result['title']}"
+        )
+
+        print(
+            f"Page ID: {result['id']}"
+        )
+
+    else:
+
+        print(
+            "\nRunbook Not Found\n"
+        )
