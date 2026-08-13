6015ed5a3170fcdcbe4b4a6b7941f2f1e6b54c36
Hussain Yandrapally <hussain.yandrapally@accenture.com>
2026-08-14 00:24:47 +1000

    s

--- app/runbook_service/confluence_search.py
+++ app/runbook_service/confluence_search.py
@@ -1,9 +1,58 @@
+import requests
+
+from app.runbook_service.confluence_client import (
+    CONFLUENCE_URL,
+    ATLASSIAN_EMAIL,
+    ATLASSIAN_API_TOKEN
+)
+
+
+def find_page_by_title(
+    title: str
+):
+
+    url = f"{CONFLUENCE_URL}/api/v2/pages"
+
+    response = requests.get(
+        url,
+        auth=(
+            ATLASSIAN_EMAIL,
+            ATLASSIAN_API_TOKEN
+        )
+    )
+
+    response.raise_for_status()
+
+    data = response.json()
+
+    for page in data.get(
+        "results",
+        []
+    ):
+
+        if (
+            page["title"].lower()
+            == title.lower()
+        ):
+
+            return {
+                "id": page["id"],
+                "title": page["title"],
+                "parent": page.get(
+                    "parentId"
+                )
+            }
+
+    return None
+
+
 if __name__ == "__main__":
 
     pages_to_find = [
         "Kubernetes",
         "CI-CD",
-        "Terraform"
+        "Terraform",
+        "CrashLoopBackOff"
     ]
 
     for page_name in pages_to_find:
@@ -12,12 +61,12 @@ if __name__ == "__main__":
             page_name
         )
 
-        print("\n-------------------")
+        print(
+            "\n----------------------"
+        )
 
         print(
             f"Searching: {page_name}"
         )
 
-        print(
-            result
-        )
+        print(result)
