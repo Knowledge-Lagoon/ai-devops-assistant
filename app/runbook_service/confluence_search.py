d47bec34cdbd752ccaa12cfb87fb5eb11b63cf62
Hussain Yandrapally <hussain.yandrapally@accenture.com>
2026-08-14 00:43:37 +1000

    s

--- app/runbook_service/confluence_search.py
+++ app/runbook_service/confluence_search.py
@@ -6,67 +6,15 @@ from app.runbook_service.confluence_client import (
     ATLASSIAN_API_TOKEN
 )
 
+url = f"{CONFLUENCE_URL}/api/v2/folders"
 
-def find_page_by_title(
-    title: str
-):
-
-    url = f"{CONFLUENCE_URL}/api/v2/pages"
-
-    response = requests.get(
-        url,
-        auth=(
-            ATLASSIAN_EMAIL,
-            ATLASSIAN_API_TOKEN
-        )
+response = requests.get(
+    url,
+    auth=(
+        ATLASSIAN_EMAIL,
+        ATLASSIAN_API_TOKEN
     )
+)
 
-    response.raise_for_status()
-
-    data = response.json()
-
-    for page in data.get(
-        "results",
-        []
-    ):
-
-        if (
-            page["title"].lower()
-            == title.lower()
-        ):
-
-            return {
-                "id": page["id"],
-                "title": page["title"],
-                "parent": page.get(
-                    "parentId"
-                )
-            }
-
-    return None
-
-
-if __name__ == "__main__":
-
-    pages_to_find = [
-        "DevOps Runbooks",
-        "Kubernetes",
-        "CI-CD",
-        "Terraform"
-    ]
-
-    for page_name in pages_to_find:
-
-        result = find_page_by_title(
-            page_name
-        )
-
-        print(
-            "\n----------------------"
-        )
-
-        print(
-            f"Searching: {page_name}"
-        )
-
-        print(result)
+print(response.status_code)
+print(response.text)
