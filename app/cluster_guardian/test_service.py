2bc2fe15861d169cc395990b03ce46ea7bac2eda
Hussain Yandrapally <hussain.yandrapally@accenture.com>
2026-08-15 23:20:45 +1000

    s

--- app/cluster_guardian/test_service.py
+++ app/cluster_guardian/test_service.py
@@ -3,9 +3,29 @@ from app.cluster_guardian.service import (
 )
 
 CLUSTER_CONTEXT = (
-    "cloud_user@k8s-chaos-lab.us-east-1.eksctl.io"
+    "arn:aws:eks:us-east-1:992382823805:cluster/k8s-chaos-lab"
 )
 
-process_cluster(
+results = process_cluster(
     CLUSTER_CONTEXT
 )
+
+print(
+    "\n=== RESULTS ===\n"
+)
+
+for result in results:
+
+    print(
+        f"Pod: {result['pod']}"
+    )
+
+    print(
+        f"Incident: {result['incident_type']}"
+    )
+
+    print(
+        f"Runbook: {result['runbook']}"
+    )
+
+    print() 
