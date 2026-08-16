d696684cd86a5cc1d9ef33e8572f3ca620de573e
Hussain Yandrapally <hussain.yandrapally@accenture.com>
2026-08-16 22:28:20 +1000

    s

--- app/cluster_guardian/cluster_registry.py
+++ app/cluster_guardian/cluster_registry.py
@@ -1,10 +1,10 @@
 CLUSTERS = [
     {
         "name": "k8s-chaos-lab",
-        "context": "arn:aws:eks:us-east-1:992382823805:cluster/k8s-chaos-lab"
+        "context": "arn:aws:eks:us-east-1:590183823048:cluster/k8s-chaos-lab"
     },
     {
         "name": "k8s-chaos-lab-2",
-        "context": "arn:aws:eks:us-east-1:681117450266:cluster/k8s-chaos-lab-2"
+        "context": "arn:aws:eks:us-east-1:590183823048:cluster/k8s-chaos-lab-2"
     }
 ]
